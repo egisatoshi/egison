30150c8ee5ba6ef45f952f420e91ffe16277011d
Satoshi Egi <egi@egison.org>
2014-07-28 18:34:09 +0900

    implementing

--- hs-src/Language/Egison/Typing.hs
+++ hs-src/Language/Egison/Typing.hs
@@ -16,13 +16,31 @@ module Language.Egison.Typing
 
 import Prelude hiding (mapM)
 
+import Control.Arrow
+import Control.Applicative
+import Data.IORef
+import Control.Monad.Error hiding (mapM)
+import Control.Monad.State hiding (mapM, state)
+import Control.Monad.Trans.Maybe
+
 import Language.Egison.Types
 
 inferBindings :: TypeEnv -> [Binding] -> EgisonM TypeEnv
 inferBindings = undefined
 
 inferExpr :: TypeEnv -> EgisonExpr -> EgisonM EgisonType
-inferExpr = undefined
+inferExpr env (VarExpr name) = refTypeVar env name >>= liftIO . readIORef
+inferExpr env (ApplyExpr fnExpr argExpr) = do
+  fnTyp <- inferExpr env fnExpr
+  argTyp <- inferExpr env argExpr
+  case fnTyp of
+    FunctionType fnArgTyp fnRetTyp -> do
+      unify env argTyp fnArgTyp
+      return fnRetTyp
+    _ -> undefined
+  
+inferExpr _ _ = undefined
 
 unify :: TypeEnv -> EgisonType -> EgisonType -> EgisonM EgisonType
-unify = undefined
+unify _ BoolType BoolType = return BoolType
+unify _ _ _ = undefined

f2c45cc7a5014fdcfebbd025880e0b0e804b5c65
Akira KAWATA <a.kawashiro@gmail.com>
2018-03-16 17:16:16 +0900

    Made typecheck script.

--- typecheck-repl.sh
+++ typecheck-repl.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
-
-stack build && stack exec typecheck
+stack build
+if [ $# -gt 0 ]; then
+    for f in $@; do
+        stack exec typecheck -- -t $f
+    done
+else
+    stack exec typecheck
+fi
